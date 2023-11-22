3975f96f3b697a140811242ed16060b344fcdff4
HenryYenHa <henryha@live.ca>
2023-11-22 15:13:20 -0700

    Initial Upload of hidden test

---
+++ project-2-group-repo/scripts/loadItems.js
@@ -0,0 +1,14 @@
+$(function () {
+  $("#navBar").load("navBar.html");
+});
+$(function () {
+  $("#LampLight").load("LampLight.html");
+});
+
+$(function () {
+  $("#installFancySwitch").load("fancySwitch.html");
+});
+
+$(function () {
+  $("#fancyTest").load("fancyTest.html");
+});
